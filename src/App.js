2d0e9f6a7643e76146457bb8336fba0ceeb382bc
Zainal Abidin <53314006+zaiinhs@users.noreply.github.com>
2020-10-09 09:55:42 +0700

    create website village

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Curah Dringu Village.</h1>
+        <a
+          className="App-link"
+          href="https://t.me/zaiinhs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
